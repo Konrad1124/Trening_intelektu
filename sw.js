bcfec28340333dafaf616aed01f7be9e8ecef37b
Konrad1124 <konradurbanski2k@gmail.com>
2023-10-20 14:17:45 +0200

    test 1

---
+++ sw.js
@@ -0,0 +1,9 @@
+var GHPATH = '/Trening_intelektu';
+var APP_PREFIX = 'gppwa_';
+var VERSION = 'version_00';
+ 
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/styles.css`
+]
