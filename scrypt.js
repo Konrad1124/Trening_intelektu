277bf3e368a1b43a7afa5fc8eef4f351e6934df8
Konrad1124 <konradurbanski2k@gmail.com>
2023-10-31 15:27:36 +0100

    update for start

--- scrypt.js
+++ scrypt.js
@@ -1,7 +1,30 @@
-const Exit = document.getElementById("Exit")
+const Start = document.getElementById("Start");
+const Results = document.getElementById("Results");
+const Exit = document.getElementById("Exit");
+const Game1 = document.getElementById("game1");
+let GameSequence = [];
+Start.addEventListener('click', () => {
+    DailyGames();
+})
+
 
 Exit.addEventListener('click', () => {
     if (confirm("Close Window?")) {
         close();
-      }
-  })
+    }
+})
+
+function DailyGames(){
+    for (let i = 0; i < 5; i++) {
+        do {
+            let temp = Math.floor(Math.random() * 5);
+            GameSequence.forEach(element => {
+                if(element==temp){
+                    GameSequence[i]=temp;
+                }
+            })
+        } while ( GameSequence[i] != '');
+        
+    }
+    document.getElementById("Start").innerText=GameSequence;
+}
