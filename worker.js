38af1be4965553f1102a2cdca4367b2b7566d875
Konrad1124 <konradurbanski2k@gmail.com>
2024-01-24 21:34:39 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,18 +2,25 @@
 const Encoder = new TextEncoder();
 const Decoder = new TextDecoder();
 let size;
-
+let accessFile
 this.addEventListener('message', async function(e) {
     
     switch (e.data[0]) {
         case "save":
             const content = textEncoder.encode(JSON.stringify(e.data[2]));
-            const accessFile = await e.data[1].createSyncAccessHandle();
+            accessFile = await e.data[1].createSyncAccessHandle();
             size = accessFile.getSize();
             accessFile.write(content, {at: size});
+            accessFile.flush();
+
             break;
         case "read":
-            
+            size = accessHandle.getSize();
+            accessFile = await e.data[1].createSyncAccessHandle();
+            const dataView = new DataView(new ArrayBuffer(size));
+            accessFile.read(dataView);
+            textDecoder.decode(dataView)
+            this.postMessage(textDecoder.decode(dataView));
             break;
     }
 }, false);
