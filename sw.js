7d3e98465e9edbc397004510927510025c99fe03
Konrad1124 <konradurbanski2k@gmail.com>
2024-02-07 22:03:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -27,9 +27,9 @@ var URLS = [
 self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install");
   e.waitUntil((async () => {
-    const cache = await caches.open(VERSION);
+    const cache = await caches.open(VERSION).then(filesUpdate);
     console.log('[Service Worker] Caching all: app shell and content');
-    filesUpdate()
+    
 
   })());
 });
