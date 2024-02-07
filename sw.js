8c71265c6c4624f00a72db9fc0d71b2b8f24fbc5
Konrad1124 <konradurbanski2k@gmail.com>
2024-02-07 20:55:25 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/Trening_intelektu';
 var APP_PREFIX = 'TI_';
-var VERSION = 'Beta13';
+var VERSION = 'Beta14';
  
 var URLS = [    
   `${GHPATH}/`,
@@ -25,51 +25,41 @@ var URLS = [
 ]
 
 self.addEventListener("install", (e) => {
-  console.log("[Service Worker] Install");
-  e.waitUntil((async () => {
-    const cache = await caches.open(VERSION);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(URLS);
-  })());
+  function onInstall() {
+    return caches
+      .open("static")
+      .then((cache) =>
+        cache.addAll(URLS)
+      );
+  }
+ 
+  e.waitUntil(onInstall(e));
 });
 
 self.addEventListener('fetch', (e) => {
 
-  if (!(
-     e.request.url.startsWith('http:') || e.request.url.startsWith('https:')
-  )) {
-      return; 
-  }
-
-e.respondWith((async () => {
-  const request = await caches.match(e.request);
-  console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-  if (request) {
-    return request;
-  }
-  const response = await fetch(e.request);
-  const cache = await caches.open(VERSION);
-  console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-  cache.put(e.request, response.clone());
-  return response;
-})());
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      if (response) {
+        //entry found in cache
+        return response;
+      }
+      return fetch(e.request);
+    })
+  );
 
 });
 
 self.addEventListener("activate", (e) => {
-  console.log(`active`);
-  e.waitUntil(
-    caches.keys().then((keyList) => {
+  function onActivate() {
+    return caches.keys().then((keys) => {
       return Promise.all(
-        keyList.map((key) => {
-          if (key === VERSION) {
-            return;
-          }
-          return caches.delete(key);
-        }),
+        keys.filter((key) => key !== "static").map((key) => caches.delete(key))
       );
-    }),
-  );
+    });
+  }
+ 
+  e.waitUntil(onActivate(e));
 });
 
 
